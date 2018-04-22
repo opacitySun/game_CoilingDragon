53e13fd63255ddfa23707de351d6a658c8ab9490
opacitySun <809340810@qq.com>
2018-04-22 17:12:06 +0800

    1.0

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,8 @@ module.exports = {
     // 输出配置
     output: {
         // 输出路径是 myProject/webapp
-        path: path.resolve(__dirname, 'webapp'),
+        path: '/webapp',
+        publicPath : '/',
         filename: '[name].[hash].js',
         chunkFilename: '[id].[chunkhash].js'
     },
