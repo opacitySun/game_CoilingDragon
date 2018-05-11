257954334734d4550170f86035b57e72141de8d2
opacitySun <809340810@qq.com>
2018-05-11 17:12:19 +0800

    v1.0

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -19,7 +19,7 @@ config.devServer = {
     contentBase: config.output.path,
     publicPath: config.output.publicPath,
     hot: true,
-    inline: true,
+    //inline: true,
     disableHostCheck: true
 };
 config.entry.app.unshift('webpack-dev-server/client?http://localhost:'+devPort+'/','webpack/hot/dev-server');
@@ -28,7 +28,7 @@ var server = new WebpackDevServer(compiler, {
     contentBase: config.output.path,
     publicPath: config.output.publicPath,
     hot: true,
-    inline: true,
+    //inline: true,
     disableHostCheck: true
 });
 server.listen(5112);
