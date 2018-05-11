ba60e66e245a1ce82fbcf28692b5b700cbf766a1
opacitySun <809340810@qq.com>
2018-05-11 14:21:16 +0800

    v1.0

--- client/public/styles/main.css.js
+++
@@ -1,9 +0,0 @@
-// ============================
-// P1. Dependencies Styles
-// ============================
-// import "bootstrap/dist/css/bootstrap.min.css";
-
-// ============================
-// P2. Style Guideline
-// ============================
-import {} from "./common.scss";
