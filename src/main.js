1fae2dd257edb36ce2288a0f1ce7f4ea3a9c8a79
opacitySun <809340810@qq.com>
2018-05-15 13:53:54 +0800

    v1.0

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router from './router';
 import App from './containers/app';
 
+ReactDOM.render(Router, document.getElementById('root'));
 ReactDOM.render(<App />, document.getElementById('root'));
---
+++ src/router.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {BrowserRouter,HashRouter,Route} from 'react-router-dom';
+// import About from './About.jsx';
+// import Home from './Home.jsx';
+
+function Home(){
+	return <h2>Home Page</h2>
+}
+
+function About(){
+	return <h2>About Page</h2>
+}
+
+class Router extends Component {
+  render() {
+    return (
+		<BrowserRouter>
+			<div>
+				<Route path='/about' component={About} />
+				<Route path='/home' component={Home} />
+			</div>
+		</BrowserRouter>
+    );
+  }
+}
+
+export default Router;
