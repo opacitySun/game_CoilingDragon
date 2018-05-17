41995b2344daa252d19f03452dd07c8b454b5e80
opacitySun <809340810@qq.com>
2018-05-17 10:12:48 +0800

    v1.0

--- src/containers/app.js
+++ src/containers/app.js
@@ -49,7 +49,7 @@ class App extends Component {
                 </div>
               </Router>
             </div>
-            <Popup/>
+            // <Popup/>
         </div>
       </Provider>
     );
