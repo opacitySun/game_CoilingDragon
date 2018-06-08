2515f1bcfa162601841280f4902fa1c03115be37
opacitySun <809340810@qq.com>
2018-06-08 15:18:02 +0800

    v1.0

--- src/components/index.js
+++ src/components/index.js
@@ -1,25 +1,15 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import ThemeSwitch from './themeSwitch';
+import { connect } from 'react-redux';
 
 class Index extends Component {
     constructor(props) {
         super(props);
-        this.state = { themeColor: '' };
-    }
-    componentWillMount () {
-        const { store } = this.context;
-        this._updateThemeColor();
-        store.subscribe(() => this._updateThemeColor());
-    }
-    _updateThemeColor () {
-        const { store } = this.context
-        const state = store.getState()
-        this.setState({ themeColor: state.themeColor })
     }
     render () {
         return (
-            <div style={{background:this.state.themeColor}}>
+            <div style={{background:this.props.themeColor}}>
                 <h2>首页</h2>
                 <p>这是一个 react 学习的基本操作的小栗子</p>
                 <p>通过本次学习可以清楚的掌握， react-router、redux的基本使用方法</p>
@@ -33,7 +23,15 @@ class Index extends Component {
 }
 
 Index.contextTypes = {
-    store: PropTypes.object
+    themeColor: PropTypes.string
 };
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+};
+
+Index = connect(mapStateToProps)(Index);
 
 export default Index;
